a9d09fa5a8cf18f507116a328f7b23803d503a05
itsukikawamu <itsuki0204@gmail.com>
2024-10-14 16:41:17 +0900

    文字列の中の数字の出現回数を数える

--- practice.c
+++ practice.c
@@ -3,12 +3,31 @@
 
 int main(void)
 {
-    char *str = malloc( sizeof(char) * 50 );
+    char *str = malloc( sizeof(char) * 51 );
     if (str == NULL) exit(1);
 
-    printf("文字列を入力してください。\n");
-    fgets(str, 49, stdin);
-    printf("%s", str);
+    printf("文字列を入力してください。(50文字まで)\n");
+    fgets(str, 50, stdin);
+
+    int count[10] = {0};
+    int i  = 0;
+    while (str[i] != '\0')
+    {
+        if (str[i] >= '0' && str[i] <= '9')
+        {
+            count[str[i]-'0']++;
+        }
+        i++;
+    }
+
+    int j = 0;
+    printf("各数字の出現回数\n");
+    while(j < 10)
+    {
+        printf("%d: %d回\n", j, count[j]);
+        j++;
+    }
+
 
     free(str);
     return 0;
