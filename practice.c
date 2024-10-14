e474c867c80e1fdc1688cd8bbcb21a6acab8cfab
itsukikawamu <itsuki0204@gmail.com>
2024-10-14 16:18:39 +0900

    fgetsバージョン

--- practice.c
+++ practice.c
@@ -7,8 +7,8 @@ int main(void)
     if (str == NULL) exit(1);
 
     printf("文字列を入力してください。\n");
-    scanf("%s", str);
-    printf("%s\n", str);
+    fgets(str, 49, stdin);
+    printf("%s", str);
 
     free(str);
     return 0;
