9fccd4597bf8779216a871074636335636b02809
itsukikawamu <itsuki0204@gmail.com>
2024-10-14 12:54:42 +0900

    偶奇判定

--- practice.c
+++ practice.c
@@ -1,33 +1,18 @@
 #include <stdio.h>
-#include <string.h>
-#include <ctype.h>
 
-int main(int argc, char *argv[])
+int main(void)
 {
-    //コマンドラインで引数を1つとると大文字に変換
-    if (argc == 2)  
-    {
-        //大文字変換前を表示
-        printf("Before: ");
-        printf("%s\n", argv[1]); 
+    int input;
+    printf("整数を入力してください: ");
+    scanf("%d", &input);
 
-        //大文字変換後を表示
-        printf("AFTER: ");
-        for (int i =0, n = strlen(argv[1]); i < n; i++)
-        {
-            printf("%c", toupper(argv[1][i]));
-        }
-        printf("\n");
-        return 0;
-    }
-    // 引数が過少・過多なら大文字変換せず差し戻す
-    else
+    if (input % 2 == 0)
     {
-        printf("Input one argument.\n");
-        return 1;
+        printf("偶数です。\n");
     }
-    
-    
+    else 
+    {
+        printf("奇数です。\n");
+    }
+
 }
-
-
