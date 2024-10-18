286e0bee067bb0b9ec6ff940ebca41894e419a1b
itsukikawamu <itsuki0204@gmail.com>
2024-10-18 12:57:33 +0900

    バブルソ－ト

--- practice.c
+++ practice.c
@@ -1,27 +1,41 @@
 #include <stdio.h>
 
-long factorial(long n)
-{
-    if (n == 0){
-        return 1;
-    }
-    else{
-        return n * factorial(n - 1);
-    }
-}
+void printArray(int arr[], int n);
+void bubbleSort(int arr[], int n);
 
-int main(void)
-{
-    long num;
-    printf("自然数を入力してください:");
+int main(void){
+    int array[]={1,6,5,98,55,13,5,49,11,21,51};
+    int size = sizeof(array)/sizeof(array[0]);
+
+    printf("元の配列: \n");
+    printArray(array, size);
+    bubbleSort(array, size);
+    printf("昇順にソート: \n");
+    printArray(array, size);
+
     
-    while ( scanf("%ld", &num) != 1 || num < 0){
-        printf("自然数を入力してください。");
-        while(getchar()!='\n');
-        scanf("%ld", &num);
-    }
-    num = factorial(num);
-    printf("%ld\n", num);
+
 
     return 0;
+}
+
+void printArray(int arr[], int n) {
+    int i;
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+void bubbleSort(int arr[], int n) {
+    int i=0, j=0, tmp=0;
+    for (i=0; i < n-1; i++){
+        for (j = 0; j < n-i-1; j++){
+            if (arr[j] > arr[j+1]){
+                tmp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = tmp;
+            }
+        }
+    }
 }
