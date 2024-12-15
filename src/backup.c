cd96d3250811f8cdde303fe562c7976d894b9fc2
itsukikawamu <itsuki0204@gmail.com>
2024-12-15 13:03:48 +0900

    241215

--- src/backup.c
+++ src/backup.c
@@ -1,3 +1,24 @@
 #include <stdio.h>
+#include <stdint.h>
 
+typedef uint8_t BYTE;
 
+int main(int argc, char *argv[])
+{
+    FILE *src = fopen(argv[1], "rb");
+    FILE *dst = fopen(argv[2], "wb");
+    if(src==NULL || dst==NULL)
+    {
+        return 1;
+    }
+
+    BYTE b;
+    while(fread(&b, sizeof(b), 1, src) != 0)
+    {
+        fwrite(&b, sizeof(b), 1, dst);
+    }
+    
+    fclose(dst);
+    fclose(src);
+    return 0;
+}
